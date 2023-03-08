a2263dee7fb00d9c9a3d8ebdeda9c576729e07ab
haris18896 <haris18896@gmail.com>
2023-03-07 23:44:14 -0800

    some thing is wrong with webview

--- taxiTracking/babel.config.js
+++ taxiTracking/babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset', '@babel/preset-env', '@babel/preset-react'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [],
+  env: {
+    production: {
+      plugins: ['react-native-paper/babel'],
+    },
+  },
+  sourceMaps: true,
 };
